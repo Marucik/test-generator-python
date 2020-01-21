d2190f3e6e1ad74437ef8836b5dac9cb3024e8cf
Marucik <rakoczy.mk@gmail.com>
2020-01-21 10:36:42 +0100

    "Refactor" Thanks to @Dawiducik

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # test-generator-python
 
-Simple test generator made in python.
-Specify test quantity in code and run it.
-Will output test and correct test answers. ✔️
+Simple test generator made in python. \
+Specify test quantity of test in code and run it.\
+Will output test and correct answers key. ✔️
--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import random
 
 testy = 3
 
-naglowek = Template(
+headerT = Template(
     "Imie i nazwisko:\n\nData:\n\nSemest:\n\n\t\t\t\tStolice - sprawdzian(Formularz $version)\n"
 )
 
-pytanie = Template("\n$number. Jaką stolicę ma państwo $country?\n")
+questionT = Template("\n$number. Jaką stolicę ma państwo $country?\n")
 
-opcje = Template("\tA. $A\n\tB. $B\n\tC. $C\n\tD. $D\n")
+choicesT = Template("\tA. $A\n\tB. $B\n\tC. $C\n\tD. $D\n")
 
 answers = {}
 
@@ -26,37 +26,33 @@ for x in answers:
 questionAnswers = {"A": "", "B": "", "C": "", "D": ""}
 
 for i in range(1, testy + 1):
-    test = open(f"spr{i}.txt", "w")
-    key = open(f"odp{i}.txt", "w")
+    with open(f"spr{i}.txt", "w") as test, open(f"odp{i}.txt", "w") as key:
 
-    test.write(naglowek.substitute(version=i))
+        test.write(headerT.substitute(version=i))
 
-    random.shuffle(countries)
+        random.shuffle(countries)
 
-    for j in enumerate(countries):
-        test.write(pytanie.substitute(number=j[0] + 1, country=j[1]))
-        temp = answers.copy()
-        questionAnswersTemp = []
+        for j in enumerate(countries):
+            test.write(questionT.substitute(number=j[0] + 1, country=j[1]))
+            temp = answers.copy()
+            questionAnswersTemp = []
 
-        testAnswer = answers.get(j[1])
-        questionAnswersTemp.append(testAnswer)
+            testAnswer = answers.get(j[1])
+            questionAnswersTemp.append(testAnswer)
 
-        del temp[j[1]]
+            del temp[j[1]]
 
-        questionAnswersTemp = questionAnswersTemp + random.sample(
-            list(temp.values()), 3
-        )
+            questionAnswersTemp = questionAnswersTemp + random.sample(
+                list(temp.values()), 3
+            )
 
-        random.shuffle(questionAnswersTemp)
+            random.shuffle(questionAnswersTemp)
 
-        for item in enumerate(questionAnswers):
-            questionAnswers[item[1]] = questionAnswersTemp[item[0]]
+            for item in enumerate(questionAnswers):
+                questionAnswers[item[1]] = questionAnswersTemp[item[0]]
 
-        test.write(opcje.substitute(questionAnswers))
+            test.write(choicesT.substitute(questionAnswers))
 
-        for dictKey in questionAnswers:
-            if questionAnswers[dictKey] == testAnswer:
-                key.write(f"{j[0] + 1}.{dictKey}\n")
-
-    test.close()
-    key.close()
+            for dictKey in questionAnswers:
+                if questionAnswers[dictKey] == testAnswer:
+                    key.write(f"{j[0] + 1}.{dictKey}\n")
